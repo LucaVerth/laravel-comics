bda32b5ac02dfadd130ade804a44aa46a7e0abf5
LucaVerth <luca.verthuy@gmail.com>
2022-01-21 17:35:37 +0100

    Added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,63 @@
 <footer>
-
-
-
+    <div class="footer-links">
+        <div class="links">
+            <ul class="sites">
+                <h4>SITES</h4>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD magazine</a></li>
+                <li><a href="#">DC kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC PowerVisa</a></li>
+            </ul>
+            <ul class="dc">
+                <h4>DC</h4>
+                <li><a href="#">Terms of use</a></li>
+                <li><a href="#">Privacy policy (new)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshop</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul class="dc-comics">
+                <h4>DC COMICS</h4>
+                <li><a href="#">Character</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+            <ul class="shop">
+                <h4>SHOP</h4>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+        </div>
+        <div class="footer-logo">
+            <img src="/img/dc-logo-bg.png" alt="" />
+        </div>
+    </div>
+    <div class="social-wrapper">
+        <div class="footer-social">
+            <div class="footer-button">
+                <div class="button"><a href="#">sign-up now!</a></div>
+            </div>
+            <div class="social-links">
+                <div class="text">FOLLOW US</div>
+                <div class="social">
+                    <span><img src="/img/footer-facebook.png" alt="" /></span>
+                    <span><img src="/img/footer-twitter.png" alt="" /></span>
+                    <span><img src="/img/footer-youtube.png" alt="" /></span>
+                    <span><img src="/img/footer-pinterest.png" alt="" /></span>
+                    <span><img src="/img/footer-periscope.png" alt="" /></span>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
